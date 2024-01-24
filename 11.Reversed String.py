efabba456e8cff29f6bbd6d4c8cc0787d093672b
beroozgar <rajranjan0105@gamil.com>
2024-01-24 18:48:36 +0530

    first command

---
+++ 11.Reversed String.py
@@ -0,0 +1,10 @@
+name = "Bro code"
+#reversed string
+reversed_string = name[::-1]
+print(reversed_string)
+
+#slicing
+web1 = "http://google.com"
+web2 = "http://amazon.com"
+slice = slice(7,-4)
+print(web1[slice])
